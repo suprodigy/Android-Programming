9a5cc6158f331b6a98401fd6e72d88926afa309f
suprodigy <suprodigy@gmail.com>
2017-02-27 17:17:32 +0900

    chapter 25. 검색

---
+++ PhotoGallery/app/src/main/java/com/example/android/photogallery/QueryPreferences.java
@@ -0,0 +1,25 @@
+package com.example.android.photogallery;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by jr on 2017-02-27.
+ */
+
+public class QueryPreferences {
+
+    private static final String PREF_SEARCH_QUERY = "searchQuery";
+
+    public static String getStoredQuery(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(PREF_SEARCH_QUERY, null);
+    }
+
+    public static void setStoredQuery(Context context, String query) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putString(PREF_SEARCH_QUERY, query)
+                .apply();
+    }
+}
